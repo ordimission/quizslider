2a0539afbf2542f4153cc13ca92bc7850ebafe9b
fabrice <fabrice@fabrice-Vostro-230>
2016-08-22 18:33:54 +0200

    add form

--- src/Scripts/main.js
+++ src/Scripts/main.js
@@ -8,11 +8,21 @@ var getData= function(result){
     var items = [];
 
     $.each(result.slide, function(i, slide){
+           
             items.push("<li>");
-            items.push("<img src='"+slide.img.src + "'></img>");
-            $.each(slide.text, function(j, paragraph) {
-                    items.push("<p>"+paragraph+"</p>");
-            }); 		
+            
+            // image
+            doImg(slide,items);
+            
+            // text
+            doText(slide,items);
+             
+            // choice
+            doChoice(slide,items); 
+             
+            // text
+            doComment(slide,items);
+            
             items.push("<li>");
     });
     $( "<ul/>", {
@@ -22,6 +32,41 @@ var getData= function(result){
     $("title").text(result.title);
 };
     
+var doImg= function(slide, items) {
+    if (typeof slide.img !== "undefined") {
+        items.push("<img src='"+ slide.img.src + "'></img>");
+    }
+}
+
+var doText= function(slide, items) {
+    if (Array.isArray(slide.text)) {
+        $.each(slide.text, function(j, paragraph) {
+                items.push("<p>"+paragraph+"</p>");
+        }); 		
+    }
+    else {
+        items.push("<p>"+slide.text+"</p>");
+    }
+}
+
+var doChoice= function(slide, items) {
+    if (typeof slide.choice !== "undefined")  {
+        items.push("<form>");
+        $.each(slide.choice, function(j, ch) {
+                items.push("<p><input type='checkbox'>&nbsp;");
+                items.push(ch.text);
+                items.push("</p>")
+        });
+        items.push("</form>");
+    }
+}
+ 
+var doComment= function(slide, items) {
+    if (typeof slide.comment !== "undefined") {
+         items.push("<p>"+slide.comment+"</p>");
+    }
+} 
+ 
 var navigate = function(){
     $('#carousel').flexslider({
             animation: "slide",
@@ -44,3 +89,4 @@ var navigate = function(){
             }
     })
 };
+
